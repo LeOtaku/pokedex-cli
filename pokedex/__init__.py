36a92d3b507643bee05b5e7a8fd046328a8d3d96
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2018-05-23 23:57:09 +0200

    fix stupid

--- pokedex/__init__.py
+++ pokedex/__init__.py
@@ -1,4 +1,4 @@
 import os
 
-fixed_resource_path = os.path.join(os.path.split(__file__)[0], "resources")
-resource_path = os.path.join(os.sep, "tmp", "pokedex-cli")
+resource_path = os.path.join(os.path.split(__file__)[0], "resources")
+var_resource_path = os.path.join(os.sep, "tmp", "pokedex-cli")
