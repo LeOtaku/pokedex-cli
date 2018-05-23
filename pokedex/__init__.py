94652f2cedc434e39cee271bc7f5441cc8c16ce1
LeOtaku <34915973+LeOtaku@users.noreply.github.com>
2018-05-23 23:47:23 +0200

    Update __init__.py

--- pokedex/__init__.py
+++ pokedex/__init__.py
@@ -1,3 +1,4 @@
 import os
 
-resource_path = os.path.join(os.path.split(__file__)[0], "resources")
+fixed_resource_path = os.path.join(os.path.split(__file__)[0], "resources")
+resource_path = os.path.join(os.sep, "tmp", "pokedex-cli")
